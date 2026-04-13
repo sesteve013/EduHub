9be963cc1a5fef7373d501922f7f23127b4116d2
sesteve013 <sesteve013@gmail.com>
2026-04-13 09:21:06 -0400

    Update App.tsx

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div style={{ padding: 20 }}>EduHub is running 🚀</div>} />
-      </Routes>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+
+        <div style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
