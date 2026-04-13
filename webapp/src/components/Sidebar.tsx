98af87bb91528ded1c62b645aac8db7a63ef2c01
sesteve013 <sesteve013@gmail.com>
2026-04-13 09:21:53 -0400

    Update Sidebar.tsx

--- webapp/src/components/Sidebar.tsx
+++ webapp/src/components/Sidebar.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Sidebar() {
   return (
-    <div style={{ width: 200, padding: 20, background: "#111", height: "100vh" }}>
+    <div style={{ width: 200, padding: 20, background: "#111", height: "100vh", color: "white" }}>
       <h2>EduHub</h2>
+
+      <div style={{ marginTop: 20 }}>
+        <Link to="/" style={{ display: "block", marginBottom: 10 }}>Login</Link>
+        <Link to="/dashboard" style={{ display: "block", marginBottom: 10 }}>Dashboard</Link>
+      </div>
     </div>
   );
 }
