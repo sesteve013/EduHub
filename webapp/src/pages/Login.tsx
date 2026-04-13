ff98904fd00042e3a318d0cf2974677e483ead57
sesteve013 <sesteve013@gmail.com>
2026-04-13 09:22:59 -0400

    Update Login.tsx

--- webapp/src/pages/Login.tsx
+++ webapp/src/pages/Login.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
   return (
     <div style={{ padding: 20 }}>
       <h1>Login Page</h1>
+
+      <Link to="/dashboard">
+        Go to Dashboard →
+      </Link>
     </div>
   );
 }
